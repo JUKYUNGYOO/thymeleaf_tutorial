b6ab709fb21f2cbb531acaf63b89ff719ce1667b
jukyung <qhdwndb>
2023-01-09 17:13:01 +0900

    Initial commit

---
+++ src/main/java/com/example/thymeleaf_t/ThymeleafTApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaf_t;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafTApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafTApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thymeleaf_t/ThymeleafTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaf_t;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafTApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
